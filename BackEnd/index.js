784cff4e379fa631ca23114528103d0a885a99dd
Gagangowda777 <gowdagagan799@gmail.com>
2026-04-02 13:02:17 +0530

    basic server and DB setup and connection

---
+++ BackEnd/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("DB Connected");
+})
+.catch((err)=>{
+    console.log("error", err);
+})
+
+app.listen(process.env.PORT, ()=>{console.log("server connected")})
